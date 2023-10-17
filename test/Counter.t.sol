b0b43ae12440bd7f24a978422e2ce8b04c1ed8fe
Marco Besier <marcobesier@hey.com>
2023-10-17 13:39:05 +0200

    Initial test suite setup

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.20;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {BondingCurveToken} from "../src/BondingCurveToken.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract BondingCurveTokenTest is Test {
+    BondingCurveToken public bondingCurveToken;
+    address public user1;
+    address public user2;
+
+    event Buy(address indexed buyer, uint256 amount, uint256 cost);
+    event Sell(address indexed seller, uint256 amount, uint256 payout);
+
+    error TokenOwnerCooldownNotOverYet(uint256 timeSinceLastTransaction, uint256 cooldownDuration);
+    error SentValueDoesNotEqualPrice(uint256 sentValue, uint256 price);
+    error InsufficientTokensToSell(uint256 balance, uint256 amount);
+    error FailedToTransferEtherPayout();
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
-    }
-
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        user1 = address(0x123);
+        user2 = address(0x456);
+        bondingCurveToken = new BondingCurveToken("BondingCurveToken", "BCT");
     }
 }
